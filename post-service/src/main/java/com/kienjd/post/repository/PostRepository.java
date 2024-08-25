4759aa8661de13049da20b85757f5c7b366a331d
kien-fit <vankientl99@gmail.com>
2024-08-25 22:36:32 +0700

    #post-service: pagination and map/dictionary-based strategy

--- post-service/src/main/java/com/kienjd/post/repository/PostRepository.java
+++ post-service/src/main/java/com/kienjd/post/repository/PostRepository.java
@@ -1,10 +1,10 @@
 package com.kienjd.post.repository;
 
 import com.kienjd.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 public interface PostRepository extends MongoRepository<Post, String> {
-    List<Post> findAllByUserId(String userId);
+    Page<Post> findAllByUserId(String userId, Pageable pageable);
 }
